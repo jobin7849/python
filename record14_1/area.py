5fdfa50dd7c0da11eaf506abccd5abdec8354ed6
jobin7849 <jobinaugusthy123@gmail.com>
2025-10-28 12:39:50 +0530

    Completed

---
+++ record14_1/area.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from math import pi
+def rarea(l,w):
+    print("area of rectangle is : ",l*w)
+def rpmtr(l,w):
+    print("perimetr of rectangle : ",2*(l+w))
+def area(r):
+    print("area of circle : ",pi*r**2)
+def pmtr(r):
+    print("perimeter of circle : ",2*pi*r)
+
+
+# In[ ]:
+
+
+
+
